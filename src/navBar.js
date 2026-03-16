32bac567fcec8e0514d9b7dc8250b6d6efe84d7f
fadybebawy <bebawypeter7@gmail.com>
2026-03-17 00:27:11 +0200

    create header

---
+++ src/navBar.js
@@ -0,0 +1,196 @@
+import React from "react";
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import SubjectIcon from "@mui/icons-material/Subject";
+import Container from "@mui/material/Container";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import BookIcon from "@mui/icons-material/Book";
+import PhoneIcon from "@mui/icons-material/Phone";
+const NavBar = () => {
+  const [value, setValue] = useState(0);
+  const [lis, setList] = useState([
+    "home",
+    "about",
+    "services",
+    "portfolio",
+    "blog",
+    "contact",
+  ]);
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  function selectIcon(text) {
+    switch (text) {
+      case "home":
+        return <HomeIcon />;
+      case "about":
+        return <InfoIcon />;
+      case "services":
+        return <SupportAgentIcon />;
+      case "portfolio":
+        return <ContactsIcon />;
+      case "blog":
+        return <BookIcon />;
+      case "contact":
+        return <PhoneIcon />;
+      default:
+        return null;
+    }
+  }
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {lis.map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{selectIcon(text)}</ListItemIcon>
+              <ListItemText primary={text.toUpperCase()} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <AppBar
+      position="static"
+      sx={{ bgcolor: "white", color: "black", boxShadow: "0" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "orange",
+              textDecoration: "none",
+            }}
+          >
+            deneb
+          </Typography>
+
+          <Box>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                alignItems: "center",
+                fontSize: "40px",
+              }}
+            >
+              <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              >
+                <BottomNavigationAction
+                  color="orange"
+                  label="Recents"
+                  sx={{ fontWeight: "bold" }}
+                />
+                <BottomNavigationAction
+                  label="Favorites"
+                  sx={{ fontWeight: "bold" }}
+                />
+                <BottomNavigationAction
+                  label="Nearby"
+                  sx={{ fontWeight: "bold" }}
+                />
+              </BottomNavigation>
+
+              <Button
+                variant="contained"
+                color="info"
+                sx={{
+                  color: "white",
+                  bgcolor: "orange",
+                  borderRadius: "20px",
+                  m: "0 20px",
+                  transition: ".3s",
+                  boxShadow: "0",
+                  "&:hover": {
+                    transform: "translatey(-2px)",
+                    boxShadow: "0",
+                    bgcolor: "#bc6502",
+                    maxHeight: "40px",
+                  },
+                }}
+              >
+                Hire Us
+              </Button>
+            </Box>
+            <div>
+              <React.Fragment>
+                <Button
+                  sx={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "black",
+                    display: { xs: "flex", md: "none" },
+                  }}
+                  onClick={toggleDrawer("right", true)}
+                >
+                  <SubjectIcon />
+                </Button>
+                <Drawer
+                  anchor={"right"}
+                  open={state["right"]}
+                  onClose={toggleDrawer("right", false)}
+                >
+                  {list("right")}
+                </Drawer>
+              </React.Fragment>
+            </div>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
